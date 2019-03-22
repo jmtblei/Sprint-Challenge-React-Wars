6f933f47beb8a2d5f6be8143d78ce81a23ea4bb8
jmtblei <47536941+jmtblei@users.noreply.github.com>
2019-03-22 14:34:02 -0700

    added js page and rerendered species to return data within species instead of returning a URL

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -34,9 +34,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
-        <div className="card-container">
           <CharacterCard character={this.state.starwarsChars}/>
-        </div>
       </div>
     );
   }
--- starwars/src/components/Characters.js
+++ starwars/src/components/Characters.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './StarWars.css';
+import Species from './Species';
 
 const Characters = props => {
     return (
         <div className="characters">
-            <h2>Name: {props.character.name}</h2>
+            <h2>{props.character.name}</h2>
             <p>Gender: {props.character.gender}</p>
             <p>Birth Year: {props.character.birth_year}</p>
-            <p>Species: {props.character.species}</p>
+                {props.character.species.map((res, index) => (
+                <Species key={index}
+                species = {res} 
+                />
+                ))}
         </div>
     )
 }
